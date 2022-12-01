7ccd19a7ef68cc2b5f779e0272785f605835b48c
Zhanna Bohan <foto1drug@gmail.com>
2022-12-02 02:01:15 +0300

    feat: done creat.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
-const create = async () => {
-    // Write your code here 
+import fs from "node:fs";
+import { writeFile, mkdir, access } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "files", "fresh.txt");
+const fileText = "I am fresh and young";
+
+const checkIfFile = async (path) => {
+  return await access(path, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
 };
 
+const create = async () => {
+  if (await checkIfFile(filePath)) throw new Error("FS operation failed");
+  else {
+    await writeFile(filePath, fileText);
+  }
+};
 await create();
