f64b847d1e1a5dddb47a817b4f2a3c54a48c6e08
Zhanna Bohan <foto1drug@gmail.com>
2022-12-02 21:26:01 +0300

    feat: copy task done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { promises } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import checkIfFile from "../helpers/helpers.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const oldFilePath = path.join(
+  path.join(__dirname, "files"),
+  "wrongFilename.txt"
+);
+const newFilePath = path.join(
+  path.join(__dirname, "files"),
+  "properFilename.md"
+);
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    if (await (!checkIfFile(oldFilePath) || checkIfFile(newFilePath)))
+      throw new Error("FS operation failed");
+    await promises.rename(oldFilePath, newFilePath);
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 await rename();
