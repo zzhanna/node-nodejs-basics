9483fc8811caf68b65784dcddd5614631c3d3fe2
Zhanna Bohan <foto1drug@gmail.com>
2022-12-02 21:36:52 +0300

    feat: delete task done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { rm } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import checkIfFile from "../helpers/helpers.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRemovePath = path.join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    if (!(await checkIfFile(fileToRemovePath)))
+      throw new Error("FS operation failed");
+    await rm(fileToRemovePath);
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 await remove();
