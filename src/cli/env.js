1dd7a022f66c5981d859cd4d2f249ba8a5d5e178
Zhanna Bohan <foto1drug@gmail.com>
2022-12-03 14:11:25 +0300

    feat: task env.js done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const values = Object.keys(process.env)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((el) => ` ${el}=${process.env[el]};`);
+
+  return console.log(values.join("").trim().slice(0, -1));
 };
 
 parseEnv();
