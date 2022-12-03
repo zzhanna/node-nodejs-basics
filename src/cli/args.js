a8bf4349438830ca227b2da1cf268558aea3935a
Zhanna Bohan <foto1drug@gmail.com>
2022-12-03 15:33:27 +0300

    feat: task args.js done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let argsArr = [];
+  for (let i = 0; i < args.length; i += 2) {
+    argsArr.push(`${args[i].replace("--", "")} is ${args[i + 1]}`);
+  }
+  console.log(argsArr.join(", "));
 };
 
 parseArgs();
