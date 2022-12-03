02d8da3c071d71d5ee6f9ecbd3f8269c57cb8173
Zhanna Bohan <foto1drug@gmail.com>
2022-12-03 18:04:13 +0300

    feat: task hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from "node:crypto";
+import { pipeline } from "node:stream/promises";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { createReadStream } from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = createReadStream(filePath);
+  const hash = createHash("sha256");
+  try {
+    await pipeline(readStream, hash);
+    console.log(hash.digest("hex"));
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 await calculateHash();
