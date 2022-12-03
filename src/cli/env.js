8ea1bb063dc8709a00478ac545444a8febcd57e5
Zhanna Bohan <foto1drug@gmail.com>
2022-12-03 15:36:29 +0300

    fix: change fix solution in env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,9 +1,9 @@
 const parseEnv = () => {
   const values = Object.keys(process.env)
     .filter((key) => key.startsWith("RSS_"))
-    .map((el) => ` ${el}=${process.env[el]};`);
+    .map((el) => `${el}=${process.env[el]}`);
 
-  return console.log(values.join("").trim().slice(0, -1));
+  return console.log(values.join("; "));
 };
 
 parseEnv();
