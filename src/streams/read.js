c518ecc1508a52aec1ce7cba460d2e628e1b9e6a
Zhanna Bohan <foto1drug@gmail.com>
2022-12-05 01:00:34 +0300

    feat: streams read.js task done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { pipeline } from "node:stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const readFromFile = createReadStream(pathToFile, { encoding: "utf8" });
+  pipeline(readFromFile, process.stdout);
 };
 
 await read();
