5269ec8f662ac6a8b0fb3d4997bfa6a781b58288
Zhanna Bohan <foto1drug@gmail.com>
2022-12-05 01:07:13 +0300

    feat: streams write.js task done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
-const write = async () => {
-    // Write your code here 
-};
+import { createWriteStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { pipeline } from "node:stream/promises";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname, "files", "fileToWrite.txt");
+
+const write = async () => {};
+const writableFromTerminal = process.stdin;
+const writableToFile = createWriteStream(pathToFile);
+pipeline(writableFromTerminal, writableToFile);
 await write();
