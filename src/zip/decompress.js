5ea67950632b60468408742ba65e8276ad11d646
Zhanna Bohan <foto1drug@gmail.com>
2022-12-05 01:51:55 +0300

    feat: zip decompress task done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createUnzip } from "node:zlib";
+import { createWriteStream } from "node:fs";
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { pipeline } from "node:stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathFileToUnzip = path.join(__dirname, "files", "fileToCompress.txt");
+const pathFileZip = path.join(__dirname, "files", "archive.gz");
+
 const decompress = async () => {
-  // Write your code here
+  const readFile = createReadStream(pathFileZip);
+  const writeFile = createWriteStream(pathFileToUnzip);
+  const unzip = createUnzip();
+  pipeline(readFile, unzip, writeFile);
 };
 
 await decompress();
