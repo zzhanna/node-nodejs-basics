a2eeded9019e55f1f78c380e55c74556c41fdf17
Zhanna Bohan <foto1drug@gmail.com>
2022-12-05 12:48:47 +0300

    feat: task wt main.js done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { cpus } from "node:os";
+import { fileURLToPath } from "node:url";
+import { Worker, workerData } from "node:worker_threads";
+import path, { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerPath = path.join(__dirname, "worker.js");
+const numOfCpu = cpus().length;
+
 const performCalculations = async () => {
-    // Write your code here
+  const workersArray = [];
+
+  for (let i = 0; i < numOfCpu; i++) {
+    const worker = new Worker(workerPath, { workerData: 10 + i });
+    let promise = new Promise((resolve, reject) => {
+      worker.on("message", (value) =>
+        resolve({ status: "resolved", data: value })
+      ),
+        worker.on("error", () => {
+          reject({ status: "error", data: null });
+        });
+    });
+    workersArray.push(promise);
+  }
+
+  const result = await Promise.allSettled(workersArray);
+  console.log(result.map((key) => key["value"]));
 };
 
 await performCalculations();
